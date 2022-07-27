a0a24264960f85dde1192fed2216b67257133517
zhangyj <richard.yujia.zhang@gmail.com>
2022-07-27 21:20:31 +0800

    Spring Boot Initializer初始化

---
+++ src/backend/src/main/java/com/zhangrichard/easykanban/EasyKanbanApplication.java
@@ -0,0 +1,13 @@
+package com.zhangrichard.easykanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyKanbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyKanbanApplication.class, args);
+	}
+
+}
---
+++ src/backend/src/test/java/com/zhangrichard/easykanban/EasyKanbanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangrichard.easykanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyKanbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
