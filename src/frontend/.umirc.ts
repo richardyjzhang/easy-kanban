c275119b656c0c8b4b264ee56044e9a8c548b242
zhangyj <richard.yujia.zhang@gmail.com>
2022-07-28 22:29:47 +0800

    删除暂时不需要的文件

--- src/frontend/.umirc.ts
+++ src/frontend/.umirc.ts
@@ -15,21 +15,21 @@ export default defineConfig({
       redirect: '/home',
     },
     {
+      name: '登录',
+      path: '/login',
+      component: './Login',
+      hideInMenu: true,
+      layout: false,
+    },
+    {
       name: '首页',
       path: '/home',
       component: './Home',
     },
-    {
-      name: '权限演示',
-      path: '/access',
-      component: './Access',
-    },
-    {
-        name: ' CRUD 示例',
-        path: '/table',
-        component: './Table',
-    },
   ],
   npmClient: 'npm',
+  history: {
+    type: 'hash',
+  }
 });
 
