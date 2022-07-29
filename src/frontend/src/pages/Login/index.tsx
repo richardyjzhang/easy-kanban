80fb0e82873da0b290f5df6fd5a848eb6c137c70
zhangyj <richard.yujia.zhang@gmail.com>
2022-07-29 10:32:39 +0800

    代码格式化

--- src/frontend/src/pages/Login/index.tsx
+++ src/frontend/src/pages/Login/index.tsx
@@ -1,22 +1,27 @@
+import { Button } from 'antd';
 import React from 'react';
 import { request, useRequest } from 'umi';
-import { Button } from 'antd';
 
 const LoginPage: React.FC = () => {
+  const { data, run: login } = useRequest(
+    () => {
+      return request('/api/login', {
+        method: 'POST',
+        data: {
+          loginName: 'FUCK',
+          password: 'YOU',
+        },
+      });
+    },
+    { manual: true },
+  );
 
-    const { data, run: login } = useRequest(() => {
-        return request('/api/login', {method: 'POST', data: {
-            "loginName": "FUCK",
-            "password": "YOU",
-        }})
-    }, {manual: true})
-
-    return (
-        <div>
-            <Button onClick={login}>登录</Button>
-            <div>{data?.message}</div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Button onClick={login}>登录</Button>
+      <div>{data?.message}</div>
+    </div>
+  );
+};
 
 export default LoginPage;
