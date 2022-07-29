72c4fa869c46b49b6207b68f7f99ddf4e1a880ea
zhangyj <richard.yujia.zhang@gmail.com>
2022-07-29 17:39:31 +0800

    登录页样式调整

--- src/frontend/src/pages/Login/index.tsx
+++ src/frontend/src/pages/Login/index.tsx
@@ -1,7 +1,8 @@
-import { Button, Form, Input, message } from 'antd';
+import { Button, Col, Divider, Form, Input, message, Row } from 'antd';
 import { SHA256 } from 'crypto-js';
 import React from 'react';
 import { useRequest } from 'umi';
+import styles from './index.css';
 import { postLoginRequest } from './service';
 
 const LoginPage: React.FC = () => {
@@ -19,6 +20,10 @@ const LoginPage: React.FC = () => {
 
   // 密码加密
   const onFormFinished = (values: API.Login.LoginUser) => {
+    if (!values.loginName || !values.password) {
+      message.error('请输入用户名或密码');
+      return;
+    }
     login({
       ...values,
       password: SHA256(`easy-kanban${values.password}`).toString(),
@@ -26,28 +31,30 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div>
-      <Form onFinish={onFormFinished}>
-        <Form.Item
-          label="用户名"
-          name="loginName"
-          rules={[{ required: true, message: '' }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="密码"
-          name="password"
-          rules={[{ required: true, message: '' }]}
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            登录
-          </Button>
-        </Form.Item>
-      </Form>
+    <div className={styles.root}>
+      <Row className={styles.loginWrapper} justify="end" align="middle">
+        <Col>
+          <div className={styles.loginCard}>
+            <div className={styles.loginTitle}>easy-kanban</div>
+            <Divider className={styles.loginDivider} />
+            <Form onFinish={onFormFinished}>
+              <Form.Item name="loginName">
+                <Input placeholder="请输入用户名" />
+              </Form.Item>
+              <Form.Item name="password">
+                <Input.Password placeholder="请输入密码" />
+              </Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className={styles.loginBtn}
+              >
+                登录
+              </Button>
+            </Form>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
