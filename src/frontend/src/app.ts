30a298cd6d671c06fac9f9cc189d4308923aa9cf
zhangyj <richard.yujia.zhang@gmail.com>
2022-07-29 20:30:56 +0800

    request请求失败的两种提示及处理

--- src/frontend/src/app.ts
+++ src/frontend/src/app.ts
@@ -1,10 +1,31 @@
 // 运行时配置
+import { message } from 'antd';
+import { AxiosError, history, RequestConfig } from 'umi';
 
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://next.umijs.org/docs/api/runtime-config#getinitialstate
 export async function getInitialState(): Promise<{ name: string }> {
   return { name: '@umijs/max' };
 }
+
+// request配置
+export const request: RequestConfig = {
+  errorConfig: {
+    errorHandler(err) {
+      switch ((err as AxiosError).response?.status) {
+        case 403:
+          message.error('禁止访问');
+          break;
+        case 401:
+          message.error('登录失效，请重新登录');
+          history.push('/login');
+          break;
+        default:
+          message.error('操作失败');
+      }
+    },
+  },
+};
 
 export const layout = () => {
   return {
