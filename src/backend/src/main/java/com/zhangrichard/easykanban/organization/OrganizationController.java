e6eb7aa6c57281c5f90a76fc1b089c7d709e1350
zhangyj <richard.yujia.zhang@gmail.com>
2022-08-09 22:16:22 +0800

    组织增删改查

--- src/backend/src/main/java/com/zhangrichard/easykanban/organization/OrganizationController.java
+++ src/backend/src/main/java/com/zhangrichard/easykanban/organization/OrganizationController.java
@@ -1,11 +1,11 @@
 package com.zhangrichard.easykanban.organization;
 
 import com.zhangrichard.easykanban.security.entity.LoginUser;
+import com.zhangrichard.easykanban.utils.IdWorker;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
@@ -16,6 +16,9 @@ public class OrganizationController {
     @Autowired
     OrganizationMapper  organizationMapper;
 
+    @Autowired
+    IdWorker idWorker;
+
     @GetMapping("/organizations")
     public List<Organization> getOrganizations(HttpServletResponse response) {
         // 该接口仅允许系统管理员访问
@@ -23,9 +26,92 @@ public class OrganizationController {
         LoginUser user = (LoginUser) subject.getPrincipal();
         if (!user.getSysAdmin()) {
             response.setStatus(403);
+            return null;
         }
 
         List<Organization> organizations = organizationMapper.getOrganizations();
         return organizations;
+    }
+
+    @PostMapping("/organizations")
+    public Organization addOrganization(
+            @RequestBody Organization organization, HttpServletResponse response) {
+        // 该接口仅允许系统管理员访问
+        Subject subject = SecurityUtils.getSubject();
+        LoginUser user = (LoginUser) subject.getPrincipal();
+        if (!user.getSysAdmin()) {
+            response.setStatus(403);
+            return null;
+        }
+
+        // 名字必填
+        String name = organization.getName();
+        if (name == null || name.isBlank()) {
+            response.setStatus(400);
+            return null;
+        }
+
+        // 名字不允许重复
+        Integer cnt = organizationMapper.countOrganizations(organization.getName());
+        if (cnt > 0) {
+            response.setStatus(400);
+            return null;
+        }
+
+        // 生成ID
+        Long id = idWorker.nextId();
+        organization.setId(id);
+
+        // 插入数据库
+        organizationMapper.addOrganization(organization);
+
+        return organization;
+    }
+
+    @DeleteMapping("/organizations/{id}")
+    public void deleteOrganization(
+            @PathVariable(name="id") Long id, HttpServletResponse response) {
+        // 该接口仅允许系统管理员访问
+        Subject subject = SecurityUtils.getSubject();
+        LoginUser user = (LoginUser) subject.getPrincipal();
+        if (!user.getSysAdmin()) {
+            response.setStatus(403);
+            return;
+        }
+
+        organizationMapper.deleteOrganization(id);
+    }
+
+    @PutMapping("/organizations/{id}")
+    public Organization updateOrganization(
+            @PathVariable(name="id") Long id,
+            @RequestBody Organization organization,
+            HttpServletResponse response) {
+        // 该接口仅允许系统管理员访问
+        Subject subject = SecurityUtils.getSubject();
+        LoginUser user = (LoginUser) subject.getPrincipal();
+        if (!user.getSysAdmin()) {
+            response.setStatus(403);
+            return null;
+        }
+
+        // 查找修改前的数据
+        Organization original = organizationMapper.getOneOrganizations(id);
+        if (original == null) {
+            response.setStatus(400);
+            return null;
+        }
+
+        // 名字不允许重复
+        Integer cnt = organizationMapper.countOrganizations(organization.getName());
+        if (!organization.getName().equals(original.getName()) && cnt > 0) {
+            response.setStatus(400);
+            return null;
+        }
+
+        organization.setId(id);
+        organizationMapper.updateOrganization(organization);
+
+        return organization;
     }
 }
--- src/backend/src/main/java/com/zhangrichard/easykanban/organization/OrganizationMapper.java
+++ src/backend/src/main/java/com/zhangrichard/easykanban/organization/OrganizationMapper.java
@@ -1,7 +1,9 @@
 package com.zhangrichard.easykanban.organization;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -10,4 +12,23 @@ public interface OrganizationMapper {
 
     @Select("SELECT * FROM organization WHERE deleted = 0")
     List<Organization> getOrganizations();
+
+    @Select("SELECT * FROM organization WHERE id = #{id} AND deleted = 0")
+    Organization getOneOrganizations(Long id);
+
+    @Select("SELECT COUNT(1) FROM organization WHERE " +
+            "name = #{name} AND deleted = 0")
+    Integer countOrganizations(String name);
+
+    @Insert("INSERT INTO organization (id, name, remark) " +
+            "VALUES " +
+            "(#{id}, #{name}, #{remark}) ")
+    void addOrganization(Organization organization);
+
+    @Update("UPDATE organization SET deleted = 1 WHERE id = #{id}")
+    void deleteOrganization(Long id);
+
+    @Update("UPDATE organization SET name = #{name}, remark = #{remark} " +
+            "WHERE id = #{id} AND deleted = 0")
+    void updateOrganization(Organization organization);
 }
