5b818b35a09be7e3e6acde6be1fc81cc8993a67c
zhangyj <richard.yujia.zhang@gmail.com>
2024-10-19 20:41:14 +0800

    feat: 删除部分模板页面，调整项目配置

--- frontend/.umirc.ts
+++ frontend/.umirc.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
-  routes: [
-    { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
-  ],
-  npmClient: 'yarn',
+  routes: [{ path: "/", component: "index" }],
+  history: {
+    type: "hash",
+  },
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  proxy: {
+    "/api/easy-kanban": {
+      target: "http://127.0.0.1:5000/",
+      changeOrigin: true,
+      pathRewrite: { "^/api/easy-kanban": "" },
+    },
+  },
+  npmClient: "yarn",
 });
