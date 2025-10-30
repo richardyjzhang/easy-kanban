f153918255be0923d85cc391c69a82ea8f5a250a
zhangyj <richard.yujia.zhang@gmail.com>
2025-10-30 17:09:28 +0800

    feat: 删除初始化带的一堆文件

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import TestView from '../views/TestView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +7,15 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: TestView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/test',
+      name: 'test',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('../views/TestView.vue'),
     },
   ],
 })
