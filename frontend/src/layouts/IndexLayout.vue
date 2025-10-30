8a6af72047f41ace21d2a6da47ddae3b0c4b3959
ZhangYJ <richard.yujia.zhang@gmail.com>
2025-10-30 23:59:54 +0800

    feat: 一点测试样式

---
+++ frontend/src/layouts/IndexLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="h-screen flex flex-col">
+    <nav-bar />
+    <div class="grow flex shadow-md">
+      <div
+        :class="[
+          collapsed ? 'w-0' : 'w-60',
+          'h-full bg-red-300',
+          'transition-all duration-300 ease-in-out',
+        ]"
+      ></div>
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useMenuCollapseStore } from '@/stores/counter'
+import NavBar from './components/NavBar.vue'
+
+const menuCollapsed = useMenuCollapseStore()
+const { collapsed } = storeToRefs(menuCollapsed)
+</script>
+
+<style>
+@import 'tailwindcss';
+</style>
---
+++ frontend/src/layouts/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="w-full h-12 bg-green-300 border-b border-b-gray-300 flex items-center">
+    <n-button @click="handleClick">点</n-button>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { useMenuCollapseStore } from '@/stores/counter'
+import { NButton } from 'naive-ui'
+
+const menuCollapsed = useMenuCollapseStore()
+
+function handleClick() {
+  menuCollapsed.toggle()
+}
+</script>
