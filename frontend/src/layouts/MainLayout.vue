b372589807d13c1fedcff66ec332f3ed27229ded
zhangyj <richard.yujia.zhang@gmail.com>
2025-10-30 17:16:46 +0800

    feat: 添加一个layout并设置路由

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,4 @@
+<template>
+    HHHH
+    <RouterView />
+</template>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import MainLayout from '@/layouts/MainLayout.vue'
 import TestView from '../views/TestView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -10,12 +11,16 @@ const router = createRouter({
       component: TestView,
     },
     {
-      path: '/test',
-      name: 'test',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/TestView.vue'),
+      path: '/main',
+      name: 'main-layout',
+      component: MainLayout,
+      children: [
+        {
+          path: '/main/test',
+          name: 'test-view',
+          component: () => import('../views/TestView.vue'),
+        },
+      ],
     },
   ],
 })
